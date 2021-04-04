c54e0295c447658a4f1630653bf699c616706d79
KevvKo <kklein915@gmail.com>
2021-04-04 19:10:56 +0200

    add chainlink to generate a random number

--- random.sol
+++ random.sol
@@ -1,12 +1,38 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.6;
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract RandomNumberGenerator {
-
+contract RandomNumberGenerator is VRFConsumerBase{
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
     uint256 randomNumber;
 
-    constructor() public {}
+    constructor()
+    
+    VRFConsumerBase(
+        0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, 
+        0xa36085F69e2889c224210F603D836748e7dC0088
+    )
+    
+    public
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4; 
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
 
+
+    function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId){
+        require(LINK.balanceOf(address(this)) > fee, 'Transaction canceled - not enough LINK');        
+        return requestRandomness(keyHash, fee, userProvidedSeed);
+        
+    }
+    
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomNumber = randomness;
+    }
+    
+    // returns a "random" number based on an unsecure pattern
     function badRandomNumber() public returns (uint8) {
-        return uint8(uint256(keccak256(abi.encodePacked( block.timestamp))));
+        return uint8(uint256(keccak256(abi.encodePacked( block.timestamp, block.difficulty))));
     }
 }
