f7416ddc37e999d822567b03fd4bf549721bc4d2
Kevin Klein <k.klein@intershop.de>
2021-04-05 13:03:08 +0200

    update index.js to interact with contract

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ require('dotenv').config()
 const contract = require('../build/contracts/RandomNumberGenerator.json')
 const Web3 = require('web3')
 
-const { API_URL, CONTRACT_ADDRESS } = process.env
+const { API_URL, CONTRACT_ADDRESS, PUBLIC_KEY } = process.env
 const web3 = new Web3(API_URL)
 
-const randomNumberGenerator = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS)
+const randomContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS)
+
+const main = async () => {
+    getBadRandomNumber()
+    getRandomNumberBySeed(2334242)
+    getRandomNumber()
+}
+
+const getBadRandomNumber = async () => {
+    const badRandomNumber = await randomContract.methods.getBadRandomNumber().call()
+    console.log(`Random number with an unsecure implemenation : ${badRandomNumber}`)
+}
+
+const getRandomNumberBySeed = async (providedSeed) => {
+    const randomNumberWithSeed = await randomContract.methods.getRandomNumberBySeed(providedSeed).call()
+    console.log(`Random number with an user provided seed ${randomNumberWithSeed}`)
+}
+
+const getRandomNumber = async () => {
+    const randomNumber = await randomContract.methods.getRandomNumber().call()
+    console.log(`Random number as a combination of the previous methods: ${randomNumber}`)
+}
+
+main()
